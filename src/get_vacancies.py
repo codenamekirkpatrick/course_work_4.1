517d0f12ee2da707e37616f5162efbdf0751b091
admin <codenamekirkpatrick@yandex.ru>
2024-07-21 15:12:11 +0300

    add reqv

--- src/get_vacancies.py
+++ src/get_vacancies.py
@@ -5,12 +5,12 @@ class GetVacanciesAPI(ABC):
     """Абстрактный класс для получения вакансии с сайта hh.ru"""
 
     @abstractmethod
-    def get_response(self, keyword, per_page):
+    def _get_response(self, keyword, per_page):
         """Абстрактный метод для получения ответа"""
         pass
 
     @abstractmethod
-    def get_vacancies(self, keyword, per_page):
+    def _get_vacancies(self, keyword, per_page):
         """Абстрактный метод для получения вакансии"""
         pass
 
